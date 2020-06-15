1414ed258e1f7ac4471f5f8d19b6b3fbd8f904f2
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-06-15 09:18:15 -0500

    morning pushups

---
+++ js/morning-warmups.js
@@ -0,0 +1,63 @@
+// For morning-warmups.html
+
+function returnArguments(arg) {
+    return arg;
+}
+console.log (returnArguments("Return the argument"))
+
+
+function squareRoot(num){
+    return Math.sqrt(num);
+}
+console.log (squareRoot(100))
+
+
+
+function diameterOfCircle(radius) {
+    return radius * 2;
+}
+console.log (diameterOfCircle(125));
+
+
+
+function numberInString(str){
+    return stringLength(str);
+}
+console.log(stringLength("hello everybody"));
+
+
+
+function stringLength(str) {
+    return str.length;
+}
+console.log(stringLength("hello everybody"));
+
+function returnNumberSeven () {
+    return (7);
+}
+console.log(returnNumberSeven());
+
+
+
+
+function countNextFive(num) {
+    console.log(num);
+    console.log(num +1);
+    console.log(num +2);
+    console.log(num +3);
+    console.log(num +4);
+
+}
+countNextFive(105);
+
+
+
+
+//var name = prompt ("Happy birthday! What is your name?");
+//function happyBirthday () {
+//    alert("Happy birthday to you.");
+//    alert(" Happy birthday to you.");
+//    alert("Happy birthday dear " + name + ",");
+//    alert("Happy birthday to you.");
+//}
+//happyBirthday();
