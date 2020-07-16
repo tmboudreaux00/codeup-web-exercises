51ff53dff99d166760267d573c102e6c613aa71f
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-07-16 16:44:41 -0500

    jquery exercises

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,5 +1,21 @@
+
+//reference code to alert on DOM load
+/**
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
-$('#hello').css({background: 'red'});
-$('.lorem').css({background: 'blue'});
+
+
+
+*/
+
+$('.all').css({border: '1px solid red'});
+
+$('li').css({fontSize: '20px'});
+
+$('.select').css({color: 'red'});
+$('h1, p, li').css({background: 'yellow'});
+
+$('#oneDiv').ready(function(){
+    alert($('#oneH1').text());
+});
