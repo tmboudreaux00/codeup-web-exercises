67209887e3d91a10cfb369003951280048b0811f
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-07-17 15:44:28 -0500

    jQuery exercises

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,21 +1,24 @@
-
-//reference code to alert on DOM load
-/**
 $(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+ "use strict"
+
+//Add jQuery code that will change the background color of an h1 element when clicked.
+ $('#btn1').click(function() {
+  $('#oneH1').toggleClass('h1Background');
+ });
+
+ //Make all paragraphs have a font size of 18px when they are double clicked.
+ $('p').dblclick(function() {
+  $(this).toggleClass('font18');//(this) allows individual selection vs ('p') choosing all
+ });
+ //Set all li text color to red when the mouse is hovering; reset to black when it is not.
+ $('li').hover(function() {
+  $(this).addClass('liTextHover');
+ }, function() {
+  $(this).removeClass('liTextHover');
+ });
+
+
+//Closing braces for ready listener
 });
 
 
-
-*/
-
-$('.all').css({border: '1px solid red'});
-
-$('li').css({fontSize: '20px'});
-
-$('.select').css({color: 'red'});
-$('h1, p, li').css({background: 'yellow'});
-
-$('#oneDiv').ready(function(){
-    alert($('#oneH1').text());
-});
