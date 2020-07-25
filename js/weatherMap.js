e109a120703fc81c0dbc40e114443c70df9e3df4
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-07-24 23:56:01 -0500

    updates

--- js/weatherMap.js
+++ js/weatherMap.js
@@ -12,65 +12,97 @@
             units: "imperial",
             
 
-            lang: "de"
+            //lang: "de"
         }).done(function(data) {
-            let unixUTC = data.TIMECHANGEME;
-            let humanDate = new Date(unixUTC * 1000);
+            //UNIX date/time stamps converter
+            let currentUTC = data.current.dt;
+            var hour;
+            var timeConverter = function(currentUTC){
+                var a = new Date(currentUTC * 1000);
+                var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+                var year = a.getFullYear();
+                var month = months[a.getMonth()];
+                var date = a.getDate();
+                hour = a.getHours();
+                var min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
+                var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
+                return time;
+            }
 
-            $('#weatherdata').html("");
-            $('#weatherdata').append('<div class="container-fluid">'
+            //location converter
+            let userLocation = "";
+
+
+           // + '<div>' + '<span>Current Temperature For </span>' + "San Antonio"/**data.name*/ + '</div>'
+            let currentDailyHigh;
+            let currentDailyLow;
+            let noExtremeTemp = function() {
+                if (hour < 18) {
+                    currentDailyHigh = data.daily[0].temp.max;
+                } else {
+                    currentDailyHigh = "--";
+                }
+
+                if (hour != 6) {
+                    currentDailyLow = data.daily[0].temp.min;
+                } else {
+                    currentDailyLow = "--"
+                }
+            }
+            noExtremeTemp();
+            let coolerWarmer = function () {
+                return "UNFINISHED"
+            }
+            data.daily.forEach(function(days){
+
+                $('#weatherdata').html("");
+                $('#weatherdata').append('<div class="container-fluid">'
+
                 + '<div class="row">'
                 + '<div class="card" style="width: 18rem;">'
-                + '<div>' + data.daily.dt + '</div>'
-                + '<div>' + '<span>Current Temperature In: </span>' + data.name + '</div>'
-                + '<div>' + data.current.temp + '<span>&deg;F</span>' + '</div>'
-                + '<div>' + '<span>' + '</span>' + '</div>'
-                + '<div>' + '<span>' + '</span>' + '</div>'
-                + '<div>' + '<span>' + '</span>' + '</div>'
-                + '<div>' + '<span>' + '</span>' + '</div>'
-                + '<div>' + '<span>' + '</span>' + '</div>'
-                + '<div>' + '<span>' + '</span>' + '</div>'
-                + '</div>'
-                + '</div>'
-    );
-            /**
-            //date
+                + '<div>' + timeConverter(currentUTC) + '</div>'
+                + '<div class="row">'
 
-            //hi degrees f/lo degrees f
-            //forecast icon
-
-            data.weather.//description: overcast clouds
-            //humidity: 52
-
-            //wind: 6.78 speed direction directionicon
-
-            //pressure: 1012
+                + '<div class="col-9">' + '<span class="border border-warning rounded-circle">' + '<div class="col">'
+                + '<div class="row">' + currentDailyHigh + '<span>&deg;</span>' + '<span class="divider">|</span>' + currentDailyLow + '<span>&deg;</span>' + '</div>'
+                + '<div class="row">' + data.current.temp + '<span>&deg;F</span>' + '</div>'
+                + '<div class="row">' + 'LIKE ' + data.current.feels_like + '<span>&deg;</span>' + '</div>'
 
 
-            data.forEach(function(days){
-                $('#weatherdata').append('<div class="container-fluid">'
-                    + '<div class="row">'
-                        + '<div class="card" style="width: 18rem;">'
-                            + '<div>' + days.//date + '</div>'
-                            + '<div>' + days.//hitemp + '</div>'
-                            + '<div>' +  + '</div>'
-                            + '<div>' +  + '</div>'
-                            + '<div>' +  + '</div>'
-                            + '<div>' +  + '</div>'
-                            + '<div>' +  + '</div>'
-                            + '<div>' +  + '</div>'
-                            + '<div>' +  + '</div>'
-                            + '<img src="" class="card-img-top" alt="">'
-                            + '<div class="card-body">'
-                                + '<h5 class="card-title">' + '' + '</h5>'
-                                + '<p class="card-text">' + '' + '</p>'
-                                + '<a href="#" class="btn btn-primary">' + '' + '</a>'
-                            + '</div>'
-                        + '</div>'
-                    + '</div>'
-                    + '</div>');
-            }) */
+                + '</div>'//col with temps
+                + '</span>'//rounded-circle
+                + '</div>'//col-9
+
+                + '<div class="col-3">'
+                    + '<div class="row">' + 'cloud icon' + '</div>' + 'cloudy' + '<div class="row">' + 'wind compass' + '<div class="row">' + 'Gusts 7 mph' +  '</div>' + '<div class="row">' + '</div>' + '</div>'
+                + '</div>'//col-3
+
+                + '<div class="col">' + "Tomorrow's temperature is forecast to be " + coolerWarmer() + " than today." + '</div>'
+                + '</div>' //row below date/time
+                + '</div>' //circle temp card
+
+                + '<div class="card" style="width: 18rem;">'
+                + '<div>' + '<span>' + '</span>' + '</div>'
+                + '<div>' + '<span>' + '</span>' + '</div>'
+                + '</div>' //map card
+                + '</div>' //row containing card
+                );
+            });
             console.log(data);
+
+            /**
+             //date
+
+             //hi degrees f/lo degrees f
+             //forecast icon
+
+             data.weather.//description: overcast clouds
+             //humidity: 52
+
+             //wind: 6.78 speed direction directionicon
+
+             //pressure: 1012 */
+
         });
     });
 
