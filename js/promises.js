5355323d30e8edbd105e940c0445ea92014dd7c6
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-07-31 16:09:00 -0500

    work on promises

--- js/promises.js
+++ js/promises.js
@@ -1,29 +1,19 @@
-(function() {
+(function () {
     "use strict";
-    function getGithubUsernames() {
-        return fetch('https://api.github.com/users/tmboudreaux00', {headers: {'Authorization': GITHUB_PAT}})
-        .then(response => response.json())
+    var ud = $('#userData');
+    var userName = 'tmboudreaux00';//can be a prompt
+    var getGithubUsernames = function (user) {
+        user = userName;
+        return fetch('https://api.github.com/users/' + user, {headers: {'Authorization': 'token ' + GITHUB_PAT}})
+            .then(response => response.json())
+            .then(users => {
+                console.log(typeof users.updated_at);
+                var returnData = 'User ' + userName + ' last updated on ' + users.updated_at;
+                ud.html('');
+                ud.append(returnData)
+            });
     }
-
-    getGithubUsernames().then( users => {
-        console.log(users);
-        console.log(users.login);
-        //     users.forEach( userObj => {
-    //         // do something with each username
-    //         console.log(userObj.login);
-        })
-     .catch(error => console.error(error));
-    //Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made.
-
-    const myPromise = new Promise((resolve, reject) => {
-        if (Math.random() > 0.5) {
-            resolve();
-        } else {
-            reject();
-        }
-    });
-    myPromise.then(() => console.log('resolved!'));
-    myPromise.catch(() => console.log('rejected!'));
+    getGithubUsernames();
 
 
     //IIFFE closing
