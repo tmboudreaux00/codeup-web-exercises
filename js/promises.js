443d06db8d564b9279582de33598b8bd4978b51c
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-08-01 19:38:10 -0500

    weathermap

--- js/promises.js
+++ js/promises.js
@@ -4,17 +4,33 @@
     var userName = 'tmboudreaux00';//can be a prompt
     var getGithubUsernames = function (user) {
         user = userName;
-        return fetch('https://api.github.com/users/' + user, {headers: {'Authorization': 'token ' + GITHUB_PAT}})
+        return fetch('https://api.github.com/users/' + user + '/events/public', {headers: {'Authorization': 'token ' + GITHUB_PAT}})
             .then(response => response.json())
             .then(users => {
-                console.log(typeof users.updated_at);
-                var returnData = 'User ' + userName + ' last updated on ' + users.updated_at;
+                console.log(typeof users[0].created_at);
+                var returnData = 'User ' + userName + ' last updated on ' + users[0].created_at;
                 ud.html('');
                 ud.append(returnData)
             });
     }
     getGithubUsernames();
 
+    let num;
 
-    //IIFFE closing
+    var wait = function(num) {
+        num = 1000;
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (Math.random() > 0.1) {
+                    resolve("Resolved");
+                }
+                else
+                    reject("Rejected");
+            // wait(num).then(() => resolve(alert('RESOLVED!')));
+            }, num);
+        });
+        wait.wait(num).then( console.log("in seconds"));
+    }
+
+//IIFFE closing
 })();
