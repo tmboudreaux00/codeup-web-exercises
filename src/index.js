a01f54b42811333392245df570d299ad0c1e1174
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-08-03 15:46:46 -0500

    npm set up

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const sayHello = p => console.log("Helloooooo");
+sayHello();
+
+const newSayHello = require('./say-hello.js');
---
+++ src/say-hello.js
@@ -0,0 +1,3 @@
+const sayHello2 = p => console.log("Hello2");
+sayHello2();
+//module.exports = sayHello2();
